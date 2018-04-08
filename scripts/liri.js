6fd14e2d432077c46e1d48066ecf1e34876a54a1
Charles Goodwin <cgoodwin2127@gmail.com>
2018-04-08 19:02:00 -0400

    should be done

---
+++ scripts/liri.js
@@ -0,0 +1,5 @@
+var driri = require("./driri.js");
+var inputs = process.argv.slice(2)
+
+console.log("\n\tLiri is on vacation.\nPlease use Driri.js instead. \n Nonetheless, your answer is\n\n");
+// console.log(inputs)
